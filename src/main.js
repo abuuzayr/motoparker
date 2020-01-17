5e10ca2a1bb9cdbfec678b784fb368502ff4f47f
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-01-18 00:37:16 +0800

    Add vuex store

--- src/main.js
+++ src/main.js
@@ -1,9 +1,26 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App.vue'
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 Vue.config.productionTip = false
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    location: null
+  },
+  mutations: {
+    setLocation: (state, location) => state.location = location,
+  },
+  actions: {
+    setLocation ({ commit }, location) {
+      commit('setLocation', location)
+    }
+  }
+})
 
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')
