1eb589a1fe15ab85614f929b586340dd2dd8683c
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-01-19 10:40:02 +0800

    Initialise slideout panel

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import App from './App.vue'
 import 'mapbox-gl/dist/mapbox-gl.css';
+import VueSlideoutPanel from 'vue2-slideout-panel';
 
 Vue.config.productionTip = false
 Vue.use(Vuex)
+Vue.use(VueSlideoutPanel);
 
 const store = new Vuex.Store({
   state: {
