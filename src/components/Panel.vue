4cee11df90c2436894ac4630c5d31d59cb18314e
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-02-09 00:41:50 +0800

    Update panel component:
    - Add back text to button
    - Set data keys to capitalise or uppercase
    - Add icons for boolean fields
    - Add button for editing
    - Sort data fields
    - Update styling
    - Add update date to bottom

--- src/components/Panel.vue
+++ src/components/Panel.vue
@@ -1,6 +1,8 @@
 <template>
   <fragment>
-    <font-awesome-icon :icon="['fas', 'arrow-left']" size="1x" class="close" @click="closePanel" />
+    <div class="close" @click="closePanel">
+      <font-awesome-icon :icon="['fas', 'arrow-left']" size="1x" /> back
+    </div>
     <fragment v-if="data">
       <div v-if="location">
         <h2>
@@ -9,13 +11,31 @@
         <table>
           <tr v-for="(value, key) in filteredData" :key="key">
             <td>
-              {{ key }}
+              {{ ['ura', 'hdb'].includes(key) ? key.toUpperCase() : capitalize(key) }}
             </td>
-            <td>
+            <td v-if="['ura', 'hdb', 'free'].includes(key)">
+              <font-awesome-icon :icon="['fas', 'check']" size="1x" :style="{ 'color': 'var(--green)' }" v-if="value"/>
+              <font-awesome-icon :icon="['fas', 'times']" size="1x" :style="{ 'color': 'var(--red)' }" v-else/>
+            </td>
+            <td v-else>
               {{ value }}
             </td>
           </tr>
         </table>
+        <div class="bottom">
+          <span>
+            <strong>Last updated on:</strong> 
+            {{ date }}
+          </span>
+          <button class="edit" @click="edit" v-if="this.$store.state.user">
+            <font-awesome-icon :icon="['fas', 'pen']" size="xs" class="icon" />
+            edit this location
+          </button>
+          <button class="edit" @click="login" v-else>
+            <font-awesome-icon :icon="['fas', 'sign-in-alt']" size="s" class="icon" />
+            sign in to edit
+          </button>
+        </div>
       </div>
       <div v-else-if="info" >
         <h2>
@@ -41,19 +61,26 @@ import Terms from '../data/terms.json'
 // Icons
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import { faArrowLeft, faPen, faCheck, faTimes, faSignInAlt } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 library.add(
-  faArrowLeft
+  faArrowLeft,
+  faPen,
+  faCheck,
+  faTimes,
+  faSignInAlt
 )
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 const dataToShow = [
   'directions',
+  'description',
   'charges',
-  'updatedAt',
+  'free',
+  'ura',
+  'hdb',
 ]
 
 export default {
@@ -72,6 +99,16 @@ export default {
   methods: {
     closePanel() {
       this.$emit('closePanel')
+    },
+    edit() {
+      alert('editing!')
+    },
+    login() {
+      this.$emit('closePanel')
+      this.$modal.show('login')
+    },
+    capitalize(str) {
+      return str.slice(0,1).toUpperCase() + str.slice(1)
     }
   },
   computed: {
@@ -82,8 +119,16 @@ export default {
           obj[key] = this.data[key]
         }
       })
-      return obj
-    }
+      const sortedObj = {}
+      dataToShow.forEach(key => {
+        sortedObj[key] = obj[key]
+      })
+      return sortedObj
+    },
+    date: function () {
+      if (!(this.data && this.data.updatedAt)) return false
+      return new Date(this.data.updatedAt).toDateString()
+    },
   },
   async mounted() {
     if (this.location) {
@@ -102,6 +147,8 @@ h2 {
   line-height: 1.5rem;
   border-bottom: 1px solid var(--gray);
   padding-bottom: 1.5rem;
+  padding-top: 5px;
+  clear: both;
 }
 table {
   text-align: left;
@@ -131,9 +178,29 @@ p {
 }
 
 .close {
-  position: absolute;
-  top: 10px;
-  left: 10px;
+  display: block;
+  padding: 10px;
   cursor: pointer;
+  float: left;
+  font-size: 0.8rem;
+}
+
+.bottom {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 0.8rem;
+  padding-left: 10px;
+}
+
+.button svg {
+  vertical-align: middle;
+}
+
+.edit {
+  border: none;
+  cursor: pointer;
+  display: block;
+  margin: 10px 5px;
 }
 </style>
